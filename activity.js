af5d3a3c478b6aa847829abca9518d2ea6d59020
FelipeCarvalho-Multi3dro <104569062+FelipeCarvalho-Multi3dro@users.noreply.github.com>
2025-07-18 11:45:06 -0300

    update 01

--- activity.js
+++ activity.js
@@ -1,22 +1,24 @@
 var connection = new Postmonger.Session();
+var payload = {};
 
 connection.trigger('ready');
 
 connection.on('requestedSchema', function(schema) {
     // schema retorna os atributos disponíveis no Journey
-    console.log('Schema', schema);
+    console.log('Schema', JSON.stringify(schema));
 
     // Exemplo de construção dinâmica do dropdown
-    schema.forEach(attr => {
-        const option = document.createElement('option');
-        option.value = `{{${attr.key}}}`;
-        option.text = attr.name;
-        document.getElementById('selectTituloCliente').appendChild(option);
-    });
+    // schema.forEach(attr => {
+    //     const option = document.createElement('option');
+    //     option.value = `{{${attr.key}}}`;
+    //     option.text = attr.name;
+    //     document.getElementById('selectTituloCliente').appendChild(option);
+    // });
 });
 
 connection.on('initActivity', function(data){
     //document.querySelector('#configJson').value = JSON.stringify(data, null, 2);
+    payload = data;
 });
 
 connection.on('clickedNext', function(){
